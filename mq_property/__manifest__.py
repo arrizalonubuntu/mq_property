cf9bdff9a3bbce5eb7f3686ba3a01bf5a9f1a8fe
arrizalonubuntu <61959019+arrizalonubuntu@users.noreply.github.com>
2020-03-12 13:36:33 +0700

    changing name
    merubah 'name'

--- mq_property/__manifest__.py
+++ mq_property/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "mq_property",
+    'name': "Property Rumah TIM C",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
